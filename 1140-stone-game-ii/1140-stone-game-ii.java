b5c36085f41bb42406ff50e42b72d21f07a95fdd
Helix-styx <harshsinghnyc@gmail.com>
2026-08-16 19:26:18 +0530

    Time: 3 ms (75.29%), Space: 44.4 MB (36.67%) - LeetHub

--- 1140-stone-game-ii/1140-stone-game-ii.java
+++ 1140-stone-game-ii/1140-stone-game-ii.java
@@ -5,7 +5,6 @@ class Solution {
         int n = piles.length;
         dp = new int[n][n+1];
         suffix = new int[n+1];
-        for(int[] i: dp) Arrays.fill(i,Integer.MIN_VALUE);
         for(int i=n-1; i>=0; i--) suffix[i]=suffix[i+1]+piles[i];
 
         return solve(piles, 0, 1);
@@ -14,7 +13,8 @@ class Solution {
     public int solve(int[] piles, int i, int m){
         int n = piles.length;
         if(i>=n) return 0;
-        if(dp[i][m]!=Integer.MIN_VALUE) return dp[i][m];
+        if(dp[i][m]!=0) return dp[i][m];
+        if (2 * m >= n - i) return suffix[i];
 
         int ans = Integer.MIN_VALUE;
         for(int k =1; k<=2*m && i+k<=n; k++){
