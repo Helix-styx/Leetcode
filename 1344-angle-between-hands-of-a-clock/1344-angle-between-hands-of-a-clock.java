9bbfe5ea193479fbd93f517683fb0cb6988d21dd
Helix-styx <harshsinghnyc@gmail.com>
2026-08-20 12:41:56 +0530

    Time: 0 ms (100%), Space: 44.9 MB (46.04%) - LeetHub

---
+++ 1344-angle-between-hands-of-a-clock/1344-angle-between-hands-of-a-clock.java
@@ -0,0 +1,7 @@
+class Solution {
+    public double angleClock(int hour, int minutes) {
+        double ans = Math.abs(30*hour-5.5*minutes);
+        if(ans>180) return 180-(ans-180);
+        return ans;
+    }
+}
