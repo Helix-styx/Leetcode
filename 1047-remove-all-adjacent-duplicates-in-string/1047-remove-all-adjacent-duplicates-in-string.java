67d8edb17ccecb22a7ca841df7d40e66033701ea
Helix-styx <harshsinghnyc@gmail.com>
2026-08-25 15:25:01 +0530

    Time: 37 ms (27.35%), Space: 47.1 MB (51.71%) - LeetHub

---
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String removeDuplicates(String s) {
+        StringBuilder ans = new StringBuilder();
+        Stack<Character> st = new Stack<>();
+
+        for(int i = 0; i<s.length(); i++){
+            char ch = s.charAt(i);
+            if(!st.empty() && st.peek()==ch) st.pop();
+            else st.push(ch);
+        }
+        while(!st.empty()){
+            ans.append(st.pop());
+        }
+        return ans.reverse().toString();
+    }
+}
