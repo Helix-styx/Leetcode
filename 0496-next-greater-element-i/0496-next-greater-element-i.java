16bc99fadd068979c8c0a95d49961e82458543ab
Helix-styx <harshsinghnyc@gmail.com>
2026-08-25 15:18:18 +0530

    Time: 4 ms (74.12%), Space: 45.3 MB (36.38%) - LeetHub

---
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Stack<Integer> st = new Stack<>();
+        HashMap<Integer,Integer> m = new HashMap<>();
+        for(int i = nums2.length-1; i>=0; i--){
+            while(!st.empty() && st.peek()<nums2[i]) st.pop();
+            if(st.empty()) m.put(nums2[i],-1);
+            else m.put(nums2[i],st.peek());
+            st.push(nums2[i]);
+        }
+        int[] ans = new int[nums1.length];
+        for(int i = 0; i<nums1.length; i++){
+            ans[i] = m.get(nums1[i]);
+        }
+        return ans;
+    }
+}
